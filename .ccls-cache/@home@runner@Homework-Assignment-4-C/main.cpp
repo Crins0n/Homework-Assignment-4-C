074c1d245a1846ccdda0ad95d6404ee86e8fc576
crins0n <105857003+Crins0n@users.noreply.github.com>
2022-05-29 20:39:19 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Homework-Assignment-4-C/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+
+using std::cout;
+using std::cin;
+
+int main() {
+  
+  cout << "\n";
+  cout << "Welcome to Seabreeze Bank\n";
+  cout << "*************************\n\n";
+  cout << "1. Saving Account\n";
+  cout << "2. Checkings Account\n";
+  cout << "3. Quit\n\n";
+  cout << "Please enter a menu item (1-3) >\n";
+
+  int menu;
+  
+
+  
+}

1827b0622f2417623937978082bca69b7bc66785
crins0n <105857003+Crins0n@users.noreply.github.com>
2022-05-29 22:34:27 +0000

    Created the basic menu.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,51 @@
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
-using std::cout;
 using std::cin;
+using std::cout;
+
+double checking_balance = 2500.00, savings_balance = 1000.00;
+
+int menu;
 
 int main() {
-  
+
   cout << "\n";
-  cout << "Welcome to Seabreeze Bank\n";
-  cout << "*************************\n\n";
-  cout << "1. Saving Account\n";
-  cout << "2. Checkings Account\n";
+  cout << "\tWelcome to Seabreeze Bank\n";
+  cout << "*********************************\n\n";
+  cout << "1. Savings Account\n";
+  cout << "2. Checking Account\n";
   cout << "3. Quit\n\n";
-  cout << "Please enter a menu item (1-3) >\n";
+  cin >> menu;
+  cout << "\n\n";
 
-  int menu;
-  
+  if (menu < 1 || menu > 3) {
+    cout << "You have entered an invalid option.\n";
+    cout << "Please enter a number 1-3 > ";
+    cin >> menu;
+    cout << "\n\n";
+  }
 
-  
+  switch (menu) {
+  case 1:
+    cout << "\t\tSavings Account\n\n";
+    cout << "Please enter a menu item (1-3) >\n";
+    cout << "*********************************\n";
+    cout << "1. Withdrawal\n";
+    cout << "2. Deposit\n";
+    cout << "3. Quit\n";
+
+    break;
+
+  case 2:
+    cout << "\t\tChecking Account\n\n";
+    cout << "Please enter a menu item (1-3) >\n";
+    cout << "*********************************\n";
+    cout << "1. Withdrawal\n";
+    cout << "2. Deposit\n";
+    cout << "3. Quit\n";
+
+  case 3:
+    cout << "Thank you for banking with Seabreeze";
+  }
 }

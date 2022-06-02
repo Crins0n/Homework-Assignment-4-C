18a8be3c9362c3831e82bf26b4aa7e2679beaa76
crins0n <105857003+Crins0n@users.noreply.github.com>
2022-06-02 00:32:23 +0000

    Comments and pseudocode added.

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int menu;
 
 int main() {
   do {
+    // Main Menu
     cout << std::fixed << std::setprecision(2);
     cout << "\n";
     cout << "\tWelcome to Seabreeze Bank\n";
@@ -20,7 +21,8 @@ int main() {
     cout << "3. Quit\n\n";
     cin >> menu;
     cout << "\n\n";
-
+    
+    // User validation for the Main Menu
     if (menu < 1 || menu > 3) {
       cout << "You have entered an invalid option.\n";
       cout << "Please enter a number 1-3 > ";
@@ -32,6 +34,7 @@ int main() {
     case 1:
       int savings_menu;
       do {
+        // Savings Account Menu
         cout << "\t\tSavings Account\n\n";
         cout << "Please enter a menu item (1-3) >\n";
         cout << "*********************************\n";
@@ -41,7 +44,7 @@ int main() {
         cout << "\n\n";
         cin >> savings_menu;
         cout << "\n";
-
+        // Withdrawal selection for Savings Account
         if (savings_menu == 1) {
           cout << "How much would you like to withdraw from your savings "
                   "account: ";
@@ -64,7 +67,7 @@ int main() {
             cout << "You now have zero funds in your Savings Account.\n\n";
           }
         }
-
+        // Deposit selection for Savings Account
         if (savings_menu == 2) {
           cout << "How much would you like to deposit into your savings "
                   "account: ";
@@ -76,19 +79,20 @@ int main() {
           savings_balance += savings_amount;
           cout << "Your Savings Account Balance: " << savings_balance << "\n\n";
         }
-
+        // User validation for Savings Menu
         if (savings_menu < 1 || savings_menu > 3) {
           cout << "You have entered an invalid option.\n";
           cout << "Please enter a number 1-3 > ";
           cin >> savings_menu;
         }
 
-      } while (savings_menu != 3);
+      } while (savings_menu != 3); 
       break;
 
     case 2:
       int checking_menu;
       do {
+        // Checking Account Menu
         cout << "\t\tChecking Account\n\n";
         cout << "Please enter a menu item (1-3) >\n";
         cout << "*********************************\n";
@@ -98,7 +102,8 @@ int main() {
         cout << "\n\n";
         cin >> checking_menu;
         cout << "\n";
-
+        
+        // Withdrawal Selection for Checking Account
         if (checking_menu == 1) {
           cout << "How much would you like to withdraw from your checking "
                   "account: ";
@@ -123,7 +128,7 @@ int main() {
             cout << "You now have zero funds in your Checking Account.\n\n";
           }
         }
-
+        // Deposit Selection for Checking Account
         if (checking_menu == 2) {
           cout << "How much would you like to deposit into your Checking "
                   "Account: ";
@@ -137,7 +142,7 @@ int main() {
           cout << "Your Checking Account Balance : " << checking_balance
                << "\n\n";
         }
-
+        // User Validation for Checking Menu
         if (checking_menu < 1 || checking_menu > 3) {
           cout << "You have entered an invalid option.\n";
           cout << "Please enter a number 1-3 > ";
@@ -152,3 +157,72 @@ int main() {
     }
   } while (menu != 3);
 }
+
+/* 
+Variables:
+
+double checking_balance     Starting balance of Checking Account
+double savings_balance      Starting balance of Savings Account
+double checking_amount      Input from user for Checking Account withdrawal/deposit
+double savings_amount       Input from user for Savings Account withdrawal/deposit
+int menu                    Used for Main Menu "switch" statement
+int savings_menu            Used for Savings Account inputted menu selections. 
+int checking_menu           Used for Checking Account inputted menu selections.
+
+Pseudo-Code
+
+1. Enclose entire program in a "do/while" loop to maintain menu functionality.
+2. Create "Main Menu".
+      Welcome to Seabreeze Bank
+      1. Savings Account
+      2. Checking Account
+      3. Quit
+3. Obtain user input for int menu.
+4. Validate user input for "Main Menu" using an "if" statement.
+5. Create menu selections using a "switch" statement.
+6. Case: 1 "Savings Menu"
+      1. Withdraw
+      2. Deposit
+      3. Main Menu
+7. Obtain user input for int savings_menu.
+8. if (savings_menu == 1) 
+      Output prompt for withdrawal amount.
+      Input double savings_amount.
+      Validate withdrawal amount if user does not have enough funds ("while" loop).
+      Validate that the withdrawal amount is <= 0 ("while" loop).
+      savings_balance -= savings_amount
+      Output savings_amount
+      if savings_balance == 0, out put that user has zero funds left in account.
+9. if (savings_menu == 2)
+      Ouput prompt for deposit amount.
+      Input double savings_amount. 
+      Validate that the deposit amount is <= 0 ("while" loop).
+      savings_balance += savings_amount.
+      Output savings_amount.
+10. if (savings_menu < 1 || savings_menu > 3)
+      Validate savings_menu user input. Prompt for correct selection. 
+11. Case: 2 "Checking Menu"
+      1. Withdraw
+      2. Deposit
+      3. Main Menu
+12. Obtain user input for int checking_menu.
+13. if (checking_menu == 1) 
+      Output prompt for withdrawal amount.
+      Input double checking_amount.
+      Validate withdrawal amount if user does not have enough funds ("while" loop).
+      Validate that the withdrawal amount is <= 0 ("while" loop).
+      checking_balance -= checking_amount
+      Output checking_amount
+      if checking_balance == 0, out put that user has zero funds left in account.
+14. if (checking_menu == 2)
+      Ouput prompt for deposit amount.
+      Input double checking_amount. 
+      Validate that the deposit amount is <= 0 ("while" loop).
+      checking_balance += checking_amount.
+      Output checking_amount.
+15. if (checking_menu < 1 || checking_menu > 3)
+      Validate savings_menu user input. Prompt for correct selection.
+16: Case 3: "Thank you for banking with Seabreeze".
+
+
+*/
